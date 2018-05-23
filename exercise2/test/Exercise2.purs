00065739f741d8c160cc88319af382185626da68
Sami Rosendahl <sami.rosendahl@reaktor.com>
2018-05-23 12:10:53 +0300

    Exercise 2

--- exercise2/test/Exercise2.purs
+++ exercise2/test/Exercise2.purs
@@ -28,28 +28,28 @@ tests = do
       test "of line" do
         let shape = Line (Point {x: 0.0, y: 0.0}) (Point {x: 1.0, y: 1.0})
         Assert.equal 0.0 (area shape)
-        
-    testSkip "2 Newtype: get value out of newtype" do
+
+    test "2 Newtype: get value out of newtype" do
       let adminId = AdminId "superuser247"
       Assert.equal "superuser247" (unwrapId adminId)
-      
-    suiteSkip "3 Maybe: printing usernames" do
+
+    suite "3 Maybe: printing usernames" do
       test "Prints username if given a username" do
         let maybeUsername = Just "johndoe"
         Assert.equal "Username: johndoe" (showUsername maybeUsername)
       test "Prints 'No username' if given nothing" do
         let maybeUsername = Nothing
         Assert.equal "No username" (showUsername maybeUsername)
-      
-    suiteSkip "4 Either: either the thing we want or an error" do
+
+    suite "4 Either: either the thing we want or an error" do
       test "Prints username if given a user" do
         let userResult = Right { id: 1, username: "johndoe" }
         Assert.equal "johndoe" (showUserResult userResult)
       test "Prints the error string if given an error" do
         let userResult = Left "Could not find user"
         Assert.equal "Could not find user" (showUserResult userResult)
-        
-    suiteSkip "5 Maybe: Find largest number" do
+
+    suite "5 Maybe: Find largest number" do
       test "returns largest number" do
         Assert.equal
           (Just 176)
@@ -77,22 +77,26 @@ pi :: Number
 pi = 3.14
       
 area :: Shape -> Number
+area (Circle _ r) = pi * r * r
+area (Rectangle _ w h) = w * h
 area _ = 0.0
 
 newtype AdminId = AdminId String
 
 unwrapId :: AdminId -> String
-unwrapId _ = "fixme"
+unwrapId (AdminId x) = x
 
 largestNumber :: Array Int -> Maybe Int
-largestNumber _ = Nothing
+largestNumber nums = Array.head $ Array.reverse $ Array.sort nums
 
 showUsername :: Maybe String -> String
-showUsername _ = "fixme"
+showUsername (Just name) = "Username: " <> name
+showUsername Nothing = "No username"
 
 type User =
   { id :: Int
   , username :: String }
 
 showUserResult :: Either String User -> String
-showUserResult _ = "fixme"
+showUserResult (Right {username}) = username
+showUserResult (Left e) = e
